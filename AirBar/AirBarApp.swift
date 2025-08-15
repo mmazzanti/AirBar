be6145d689cc4ae767ca88f38323d6ac505e96fb
Matteo Mazzanti <5095191+mmazzanti@users.noreply.github.com>
2025-08-15 23:16:23 +0200

    Initial Commit

---
+++ AirBar/AirBarApp.swift
@@ -0,0 +1,32 @@
+//
+//  AirBarApp.swift
+//  AirBar
+//
+//  Created by Matteo Mazzanti on 15.08.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AirBarApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AirBarTests/AirBarTests.swift
@@ -0,0 +1,16 @@
+//
+//  AirBarTests.swift
+//  AirBarTests
+//
+//  Created by Matteo Mazzanti on 15.08.2025.
+//
+
+import Testing
+
+struct AirBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
